983b993e79355ddebef46d4cafe2acf8c6c883a3
Dhanush <dhanush1926@gmail.com>
2020-05-10 21:06:00 +0530

    Updates

---
+++ precache-manifest.13e32b989dd966dbbf722d97cf697b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae6cf1b2e3f6d3c917ca9dd81fc7fe7",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "150081f3d4c879a2d943",
+    "url": "/covid19-tracker/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "27c3b4229eefab1e9431",
+    "url": "/covid19-tracker/static/css/main.a1e97f76.chunk.css"
+  },
+  {
+    "revision": "150081f3d4c879a2d943",
+    "url": "/covid19-tracker/static/js/2.7a3dbad4.chunk.js"
+  },
+  {
+    "revision": "fdfeeb22426a2d6126e3d29d223ce94a",
+    "url": "/covid19-tracker/static/js/2.7a3dbad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c3b4229eefab1e9431",
+    "url": "/covid19-tracker/static/js/main.a82695f4.chunk.js"
+  },
+  {
+    "revision": "f8c9cf4541065677aa0e",
+    "url": "/covid19-tracker/static/js/runtime-main.1cdd9d20.js"
+  }
+]);
